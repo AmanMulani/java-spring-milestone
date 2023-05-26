89061670adae222de24e070875bc166813757356
AmanMulani <amanmulani369@gmail.com>
2023-05-26 19:29:41 +0530

    feat | AM | Initial setup of project

---
+++ src/main/java/com/example/spring_milestone_project/SpringMilestoneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_milestone_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMilestoneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMilestoneProjectApplication.class, args);
+	}
+
+}
